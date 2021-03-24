387c7bf09200a0d16b329e7155d7a2f4e36f9efa
Maria Clara Rubinger <rubinger.maria@gmail.com>
2021-03-24 12:42:18 +0000

    Mudança da baseURL para deploy na Vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 // vou criar uma instância do Axios, pra setar algumas informações que são padrão pra todas as requisições que vamos fazer pra nossa API
 export const api = axios.create({ 
-  baseURL: 'http://localhost:3000/api',///o endereço que se repete em todas as requisições
+  baseURL: 'https://dtmoney-rust.vercel.app/api',
+  
+  //baseURL: 'http://localhost:3000/api',///o endereço que se repete em todas as requisições
 })
